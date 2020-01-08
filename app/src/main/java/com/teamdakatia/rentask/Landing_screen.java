538811ea5f0e1668f639b0b741eb4e05c043a9f3
monir467 <monirsatt@gmail.com>
2020-01-09 01:49:41 +0600

    splash screen and landing screen design done!!

---
+++ app/src/main/java/com/teamdakatia/rentask/Landing_screen.java
@@ -0,0 +1,14 @@
+package com.teamdakatia.rentask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landing_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_screen);
+    }
+}
