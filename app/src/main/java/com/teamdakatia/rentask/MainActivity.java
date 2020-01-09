994b3c34e8fa34d2b3c354f4a058cfbfbe664f9b
Faysal Khan <khan.faysal83@gmail.com>
2020-01-09 16:00:00 +0600

    pulled monirs changes

--- app/src/main/java/com/teamdakatia/rentask/MainActivity.java
+++ app/src/main/java/com/teamdakatia/rentask/MainActivity.java
@@ -41,9 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
@@ -53,10 +52,4 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
