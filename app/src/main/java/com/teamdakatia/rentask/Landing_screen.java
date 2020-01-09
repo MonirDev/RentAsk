ab53d03a76c091ebf14fe933064b871257f742c0
Faysal Khan <khan.faysal83@gmail.com>
2020-01-09 13:51:50 +0600

    Change Toolber Error in Xml

--- app/src/main/java/com/teamdakatia/rentask/Landing_screen.java
+++ app/src/main/java/com/teamdakatia/rentask/Landing_screen.java
@@ -2,7 +2,9 @@ package com.teamdakatia.rentask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -16,5 +18,10 @@ public class Landing_screen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing_screen);
+    }
+
+    public void rent(View view) {
+        Intent intent = new Intent(Landing_screen.this,MainActivity.class);
+        startActivity(intent);
     }
 }
