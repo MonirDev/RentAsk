73e7335c01aad6a008769bb6d215d7177ab76aa8
User <mail.zahidhossain@gmail.com>
2020-01-12 16:44:28 +0600

    New Activity added ( Sign in / Sign up)

--- app/src/main/java/com/teamdakatia/rentask/ImageAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.teamdakatia.rentask;
-
-public class ImageAdapter {
-}
---
+++ app/src/main/java/com/teamdakatia/rentask/SignIn.java
@@ -0,0 +1,32 @@
+package com.teamdakatia.rentask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignIn extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+      textView=findViewById(R.id.newaccount);
+      button=findViewById(R.id.signin);
+
+      textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+}
+}
